11ac4318ef0e0b3edbfddc7a5d67c4a18680a42f
seidmengistu <seidmengistu88@gmail.com>
2023-07-21 00:35:29 +0300

    Ui design for Header and footer part Done

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+    <p className='about'>About us</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='mt-6'>Home</div>
+   
+    </>
+  )
+}
+
+export default Home
